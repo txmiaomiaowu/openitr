a6feea1d46cab237faf58c2c3b537e974fd6fbad
txmiaomiaowu <142644183+txmiaomiaowu@users.noreply.github.com>
2024-02-03 12:01:49 +0800

    Update merge.sh

--- sublist/merge.sh
+++ sublist/merge.sh
@@ -2,6 +2,7 @@ rm ../sources.list
 # 订阅库
 wget https://raw.githubusercontent.com/snakem982/proxypool/main/proxies.txt
 wget https://raw.githubusercontent.com/snakem982/proxypool/main/nodelist.txt
+wget https://raw.githubusercontent.com/WilliamStar007/ClashX-V2Ray-TopFreeProxy/main/combine/v2raysub.txt
 #合并文件
 ls *.txt |
 while read file_name;
@@ -17,4 +18,5 @@ mv ./alls.txt ../sources.list
 rm ./all.txt
 rm ./proxies.txt
 rm ./nodelist.txt
+rm ./v2raysub.txt
 
